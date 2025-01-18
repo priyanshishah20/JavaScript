64457eb4738fe3818336712e4d44fc6181eab68e
priyanshishah20 <92794107+priyanshishah20@users.noreply.github.com>
2025-01-18 17:15:27 +0530

    todo list using localstorage

--- Javascript_Simon/scripts/11_todolist.js
+++ Javascript_Simon/scripts/11_todolist.js
@@ -2,9 +2,7 @@ const ipElem = document.getElementById('ip-val');
 const dateElem = document.getElementById('date-val');
 // const add1Elem = document.getElementById('add');
 const opElem = document.getElementById('ip-op');
-const todo = [{name: 'Breakfast', dueDate: '25-01-2025'},
-    {name: 'Lunch', dueDate: '25-01-2025'},
-    {name: 'Dinner', dueDate: '25-01-2025'}];
+const todo = JSON.parse(localStorage.getItem('todo')) || [{name: 'Breakfast', dueDate: '25-01-2025'}, {name: 'Lunch', dueDate: '25-01-2025'}, {name: 'Dinner', dueDate: '25-01-2025'}];
 
 renderTodo();
 
@@ -15,11 +13,12 @@ function renderTodo() {
         const name = todoList.name;
         // {name, dueDate} = todoList; // destructuring shortcut
         const dueDate = todoList.dueDate;
-        const html = `<div class="main-div"><p>${name}</p><p>${dueDate}</p><button class="delete-btn" onclick="todo.splice(${i}, 1);
+        const html = `<div class="main-div"><p>${name}</p><p>${dueDate}</p><button class="delete-btn" onclick="todo.splice(${i}, 1); 
+        saveToStorage();
         renderTodo();">Delete</button></div>`;
         todohtml += html;
     }
-    console.log(todohtml);
+    //console.log(todohtml);
     opElem.innerHTML = todohtml;
 }
 function add1() {
@@ -28,9 +27,16 @@ function add1() {
     // todo.push(val);
      // todo.push({name: name, dueDate: dueDate});
      todo.push({name, dueDate}); //shorthand property
+     saveToStorage();
      console.log(todo);
      opElem.innerHTML = todo;
     
     ipElem.value = ''; // reset the text in input
+    dateElem.value = ''; // reset the date in input
     renderTodo();
+}
+
+// created a function instead of writing multiple times
+function saveToStorage() {
+    localStorage.setItem('todo', JSON.stringify(todo));
 }
